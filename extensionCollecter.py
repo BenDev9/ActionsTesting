85ef863e1224791cec9d827b64be0a99710bfb0d
BenDev9 <63582647+BenDev9@users.noreply.github.com>
2021-06-13 11:43:52 +0100

    Update extensionCollecter.py

--- extensionCollecter.py
+++ extensionCollecter.py
@@ -8,11 +8,11 @@ objects = os.listdir(cwd)
 
 for o in objects:
   if o == "extensionCollecter.py":
+    print(o)
     print("that's me")
-    print(o)
   elif os.path.isdir(cwd+o):
+    print(o)
     print("that's a dir")
-    print(o)
   else:
-    print("that's a file")
     print(o)
+    print("that's a file")
