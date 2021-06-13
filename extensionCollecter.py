756196ca55073d55c7167f22a9e57f27af00e9b7
BenDev9 <63582647+BenDev9@users.noreply.github.com>
2021-06-13 11:42:43 +0100

    Update extensionCollecter.py

--- extensionCollecter.py
+++ extensionCollecter.py
@@ -4,4 +4,15 @@ cwd = os.getcwd()
 
 print(cwd)
 
-print(os.listdir(cwd))
+objects = os.listdir(cwd)
+
+for o in objects:
+  if o == "extensionCollecter.py":
+    print("that's me")
+    print(o)
+  elif os.path.isdir(cwd+o):
+    print("that's a dir")
+    print(o)
+  else:
+    print("that's a file")
+    print(o)
