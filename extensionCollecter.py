1403309e1394710a8bab2f87f19c34d9674f638d
BenDev9 <63582647+BenDev9@users.noreply.github.com>
2021-06-14 11:55:19 +0100

    Update extensionCollecter.py

--- extensionCollecter.py
+++ extensionCollecter.py
@@ -7,13 +7,19 @@ cwd = os.getcwd()
 objects = os.listdir(cwd)
 dictionary = {}
 maintainanceFiles = ["extensionCollecter.py", "filenamDict.txt"]
-
+    
 for o in objects:
-  if not o in maintainanceFiles and not os.path.isdir(cwd+"/"+o):
+  if o in maintainanceFiles:
+    print(f"{o} is a maintainance file, Skipping!")
+    print()
+  elif os.paht.isdir(cwd+"/"+o):
+    print(f"{o} is a dir, Skipping!")
+    print()
+  else:
+    print(f"{o} is a file")
     filenamewoext = pathlib.Path(o).with_suffix("")
     dictionary.update({str(filenamewoext) : o})
+    print(f"Added {o} to the dictionary")
 
-#with open("filenameDict.txt", "w") as f:
-#  f.write(str(dictionary))
-
-sys.stderr.write(str(dictionary))
+with open("filenameDict.txt", "w") as f:
+  f.write(str(dictionary))
